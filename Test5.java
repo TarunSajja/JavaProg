aa2d55857944cbf2e94762d449d5e6280ad2d930
TarunSajja <tarunsajja@gmail.com>
2014-06-28 15:52:27 +0530

    first commit

---
+++ Test5.java
@@ -0,0 +1,14 @@
+public class Test5
+{
+public static void main (String[] args)
+{
+if (10<20)
+{
+System.out.println("HELLO");
+}
+else
+{
+System.out.println("HAI");
+}
+} 
+}
---
+++ Test7.java
@@ -0,0 +1,21 @@
+public class Test7
+{
+public static void main (String[] args)
+{
+int a,b;
+
+a = 10;
+
+b = 20;
+
+if (a == b)
+
+{
+System.out.println("HELLO");
+}
+else
+{
+System.out.println("HAI");
+}
+} 
+}
